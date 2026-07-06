bbe9685ac65f1ad282d1fdd530fb37ac1c901079
morgmatt <matteo02prada@gmail.com>
2026-07-06 16:24:23 +0200

    [Models] Moved nn blocks into respective files to avoid imports

--- classification/src/model/resnet.py
+++ classification/src/model/resnet.py
@@ -1,7 +1,105 @@
 import torch
 from torch import nn
 
-from model_blocks import ResNetConvBlock, ResNetIdentityBlock
+### Basic module: Conv2d + BatchNorm + ReLU
+class Conv2d_bn(nn.Module):
+    def __init__(self, in_filters, out_filters, kernel_size, strides, padding):
+        super().__init__()
+
+        if isinstance(kernel_size, tuple):
+            padding_val: int | tuple[int, int] = (
+                tuple(k // 2 for k in kernel_size) if padding == "same" else (0, 0)
+            )
+        else:
+            padding_val = kernel_size // 2 if padding == "same" else 0
+
+        self.conv = nn.Conv2d(in_filters, out_filters, 
+                              kernel_size = kernel_size, 
+                              stride = strides, 
+                              padding = padding_val)
+        self.bn = nn.BatchNorm2d(out_filters)
+        self.relu = nn.ReLU()
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, torch.nn.Linear):
+            torch.nn.init.xavier_uniform_(module.weight)
+            if module.bias is not None:
+                module.bias.data.zero_()
+
+        if isinstance(module, torch.nn.Conv2d):
+            torch.nn.init.xavier_uniform_(module.weight)
+            if module.bias is not None:
+                module.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        return x
+    
+
+### ResNet Identity Block: (n, n, in_channels) -> (n, n, filters[2])
+class ResNetIdentityBlock(nn.Module):
+
+    def __init__(self, in_channels, filters, kernel_size, stride=1):
+        super().__init__()
+        self.main_path = nn.Sequential(
+            Conv2d_bn(in_filters = in_channels, out_filters = filters[0], kernel_size = 1, strides = stride, padding = "valid"),
+            Conv2d_bn(in_filters = filters[0], out_filters = filters[1], kernel_size = kernel_size, strides = 1, padding = "same"),
+            Conv2d_bn(in_filters = filters[1], out_filters = filters[2], kernel_size = 1, strides = 1, padding = "valid"),
+        )
+        self.relu = nn.ReLU()
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, torch.nn.Linear):
+            torch.nn.init.xavier_uniform_(module.weight)
+            if module.bias is not None:
+                module.bias.data.zero_()
+
+        if isinstance(module, torch.nn.Conv2d):
+            torch.nn.init.xavier_uniform_(module.weight)
+            if module.bias is not None:
+                module.bias.data.zero_()
+
+    def forward(self, x):
+        y = self.relu(self.main_path(x) + x) # Skip connection after a main-path block
+        return y
+
+### ResNet Convolutional Block: (2n, 2n, in_channels) -> (n, n, filters[2])
+class ResNetConvBlock(nn.Module):
+
+    def __init__(self, in_channels, filters, kernel_size):
+        super().__init__()
+        self.main_path = nn.Sequential(
+            Conv2d_bn(in_filters = in_channels, out_filters = filters[0], kernel_size = 1, strides = 2, padding = "valid"),
+            Conv2d_bn(in_filters = filters[0], out_filters = filters[1], kernel_size = kernel_size, strides = 1, padding = "same"),
+            Conv2d_bn(in_filters = filters[1], out_filters = filters[2], kernel_size = 1, strides = 1, padding = "valid"),
+        )
+        
+        self.shortcut_path = nn.Sequential(
+            nn.Conv2d(in_channels = in_channels, out_channels = filters[2], kernel_size = 1, stride = 2),
+            nn.BatchNorm2d(filters[2]),
+        )
+
+        self.relu = nn.ReLU()
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, torch.nn.Linear):
+            torch.nn.init.xavier_uniform_(module.weight)
+            if module.bias is not None:
+                module.bias.data.zero_()
+
+        if isinstance(module, torch.nn.Conv2d):
+            torch.nn.init.xavier_uniform_(module.weight)
+            if module.bias is not None:
+                module.bias.data.zero_()
+
+    def forward(self, x):
+        y = self.relu(self.main_path(x) + self.shortcut_path(x))
+        return y
 
 class ResNet50(nn.Module):
 
